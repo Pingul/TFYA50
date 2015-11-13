065fd951490f955d7d6c7bb84b7ca984f208004a
Pingul <joel@doubleint.com>
2015-11-13 09:46:21 +0100

    Greatly improved efficiency (>50%) of the interaction potential.

--- potential.cc
+++ potential.cc
@@ -24,28 +24,22 @@ Vector3 LJPotential::interaction(const Atom& atom1, const Atom& atom2, const Sim
 
 Vector3 LJPotential::interaction(const Vector3& vector1, const Vector3& vector2, const SimulationParams& params)
 {
-	double dis2 = (vector2 - vector1)*(vector2 - vector1);
-	double dis = sqrt(dis2);
-	double dis7 = pow(dis, 7);
-	double dis6 = pow(dis2, 3);
+	double dis = sqrt((vector2 - vector1)*(vector2 - vector1));
+	static double rcut = params.cutoffDistance;
+	static double rin = 0.95*rcut;
+	if (dis>rcut)
+		return Vector3{ 0, 0, 0 };
 
-	double sigma6 = pow(sigma, 6);
-	double sigma12 = pow(sigma, 12);
-
-	double constant = ((24 * epsilon) / dis7) * (2 * sigma12 / dis6 - sigma6);
-
-	double rcut = params.cutoffDistance;
-	double rin = 0.95*rcut;
-
-	double dljrin = -48.0*epsilon / rin*(pow((sigma / rin), 12) - 0.5*pow((sigma / rin), 6));
 
 	Vector3 force; /*= (constant - dljcutoff)* (vector2 - vector1) / dis;*/
 	if (dis < rin)
-		return force= constant* (vector2 - vector1) / dis;
-	else if (dis>rcut)
-		return{ 0, 0, 0 };
-	else
-		return force = dljrin / (rin - rcut)*(dis - rcut)*(vector2 - vector1)/dis;
+	{
+		static double sigma6 = pow(sigma, 6);
+		double deriv = ((24 * epsilon) / pow(dis, 7)) * sigma6*(2 * sigma6 / pow(dis, 6) - 1);
+		return deriv*(vector2 - vector1) / dis;
+	}
+	static double dljrin = -48.0*epsilon / rin*(pow((sigma / rin), 12) - 0.5*pow((sigma / rin), 6));
+	return dljrin / (rin - rcut)*(dis - rcut)*(vector2 - vector1)/dis;	
 }
 
 double LJPotential::potentialEnergy(const Vector3& vector1, const Vector3& vector2, const SimulationParams& params)
